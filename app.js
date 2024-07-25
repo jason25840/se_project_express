94c46b4a4721fa876d9e549b86f2c3657ec33045
J.Knight <jason25840@gmail.com>
2024-07-25 07:01:16 -0400

    add test Id

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const indexRouter = require("./routes/index");
+const auth = require("./middlewares/auth");
 
+if (process.env.NODE_ENV === "test") {
+  app.use((req, res, next) => {
+    req.user = {
+      _id: "5d8b8592978f8bd833ca8133",
+    };
+    next();
+  });
+}
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -15,5 +24,6 @@ mongoose
 app.use(express.json());
 app.use("/", indexRouter);
 app.use(cors());
+app.use(auth);
 
 app.listen(PORT, () => {});
