e838ba4a020e10a8f8a6377fe0d6143a2c6f983a
J.Knight <jason25840@gmail.com>
2024-07-25 07:04:00 -0400

    relocate app declaration

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const indexRouter = require("./routes/index");
 const auth = require("./middlewares/auth");
 
+const app = express();
+const { PORT = 3001 } = process.env;
+
 if (process.env.NODE_ENV === "test") {
   app.use((req, res, next) => {
     req.user = {
@@ -12,9 +15,6 @@ if (process.env.NODE_ENV === "test") {
     next();
   });
 }
-
-const app = express();
-const { PORT = 3001 } = process.env;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
