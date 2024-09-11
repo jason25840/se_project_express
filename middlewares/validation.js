c0536121254783dde2879418321813c15f4a70d4
J.Knight <jason25840@gmail.com>
2024-09-11 07:16:39 -0400

    add missing bracket

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -19,7 +19,7 @@ const validateClothingItem = celebrate({
       "string.empty": 'The "imageUrl" field must be filled in',
       "string.uri": 'the "imageUrl" field must be a valid URL',
     }),
-    weather: Joi.string().valid("hot", "warm", "cold".required().messages({
+    weather: Joi.string().valid("hot", "warm", "cold").required().messages({
       "string.empty": 'The "weather" field must be filled in',
     }),
   }),
